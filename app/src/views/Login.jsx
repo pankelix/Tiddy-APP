04050c5b3c41692c8e3006764a4e6c21b0ff9943
Miguel Arias <miguelariasgarau@gmail.com>
2024-07-05 18:10:44 +0200

    improve styles for responsive design

--- app/src/views/Login.jsx
+++ app/src/views/Login.jsx
@@ -35,49 +35,49 @@ function Login(props) {
         props.onRegisterClick()
     }
 
-    return <Container className='flex flex-col items-center justify-center'>
+    return <Container className='flex flex-col items-center justify-center h-screen lg:flex-row lg:gap-32'>
+        <div className='flex flex-col items-center'>
+            <h1 className='text-neutral-600 text-4xl font-semibold mb-[1rem]'>Login</h1>
 
-        <article className='box-border bg-white rounded-lg max-w-screen max-h-screen py-[70px] flex flex-col items-center gap-[10px] shadow-lg shadow-slate-200 h-screen'>
-            <Form onSubmit={handleSubmit} className='select-none w-[360px] flex flex-col items-center gap-[10px]'>
+            <div>
+                <Form onSubmit={handleSubmit} className='select-none w-[360px] flex flex-col items-center gap-[10px]'>
 
-                <h1 className='text-4xl font-semibold mb-[1rem]'>Login</h1>
 
-                <Input id='email-input' type='email' placeholder='Email' className='entrance-input'>Email</Input>
+                    <Input id='email-input' type='email' placeholder='Email' className='entrance-input'>Email</Input>
 
-                <Input id='password-input' type='password' placeholder='Password' className='entrance-input'>Password</Input>
+                    <Input id='password-input' type='password' placeholder='Password' className='entrance-input'>Password</Input>
 
-                <Button type='submit' className='cursor-pointer mt-5 p-[1.8rem] border-none rounded-md text-lg text-white bg-amber-400 w-[216px] h-[30px] flex justify-center items-center'>Log In</Button>
-            </Form>
+                    <Button type='submit' className='flex items-center justify-center w-full py-3 text-lg text-white border-none rounded-md cursor-pointer px-28 bg-amber-400'>Log In</Button>
+                </Form>
+            </div>
+        </div>
 
-            {loading ?
-                <div className='absolute p-5 rounded-md w-72 bg-amber-400 top-60'>
-                    <p className='pb-4 font-bold text-center text-white'>Login may take up to 3 minutes, because the server has to initialize. After that it'll be faster.</p>
-
-                    <div className='flex justify-center'>
-                        <ClipLoader
-                            color={color}
-                            loading={loading}
-                            size={100}
-                            aria-label="Loading Spinner"
-                            data-testid="loader"
-                        />
-                    </div>
-
-                </div> : ''}
-
-            <nav className='text-sm flex justify-center gap-4 w-[100%] pt-20'>
+        <nav className='flex flex-col items-center gap-10 mt-20 text-sm text-center lg:mt-12'>
+            <div className='flex gap-4 lg:flex-col lg:gap-0'>
                 <p>Don't have an account?</p>
                 <Link className='underline underline-offset-2' onClick={handleRegisterClick}>Create new account</Link>
-            </nav>
-
-            <div className='p-4 rounded-md mt-14 bg-amber-300'>
-                <h4 className='font-semibold'>Demo account:</h4>
-                <div>
-                    <p>Email: man@sion.com</p>
-                    <p>Password: 123123123</p>
-                </div>
             </div>
-        </article>
+            <div className='flex flex-col gap-1'>
+                <p className='pb-2'>Or use our demo account:</p>
+                <p>Email: <strong>man@sion.com</strong></p>
+                <p>Password: <strong>123123123</strong></p>
+            </div>
+        </nav>
+
+        {loading ?
+            <div className='absolute p-5 rounded-md w-72 bg-amber-400 top-70'>
+                <p className='pb-4 font-bold text-center text-white'>Login may take up to 3 minutes, because the server has to initialize. After that it'll be faster.</p>
+
+                <div className='flex justify-center'>
+                    <ClipLoader
+                        color={color}
+                        loading={loading}
+                        size={100}
+                        aria-label="Loading Spinner"
+                        data-testid="loader"
+                    />
+                </div>
+            </div> : ''}
     </Container>
 }
 
--- app/src/views/Register.jsx
+++ app/src/views/Register.jsx
@@ -1,14 +1,20 @@
-import { useContext } from '../hooks'
+import logic from '../logic'
 
 import { Container, Form, Link, Input, Button } from '../library'
-
-import logic from '../logic'
+import { useContext } from '../hooks'
+import { useState } from 'react'
+import ClipLoader from 'react-spinners/ClipLoader'
 
 function Register(props) {
     const context = useContext()
 
+    const [loading, setLoading] = useState(false)
+    const [color, setColor] = useState('#fff')
+
     const handleSubmit = async event => {
         event.preventDefault()
+
+        setLoading(true)
 
         const name = event.target.querySelector('#name-input').value
         const email = event.target.querySelector('#email-input').value
@@ -18,6 +24,7 @@ function Register(props) {
             await logic.registerHome(name, email, password)
             props.onSuccess()
             context.handleConfirm('Home registered', null)
+            setLoading(false)
         } catch (error) {
             context.handleError(error)
         }
@@ -28,26 +35,47 @@ function Register(props) {
         props.onLoginClick()
     }
 
-    return <Container className='flex items-center justify-center '>
-        <article className='box-border bg-white rounded-lg max-w-screen-md py-[70px] flex flex-col items-center gap-[10px] shadow-lg shadow-slate-200 h-screen'>
-            <Form onSubmit={handleSubmit} className='select-none w-[360px] flex flex-col items-center gap-[10px]'>
+    return <Container className='flex flex-col items-center justify-center h-screen lg:flex-row lg:gap-32'>
+        <div className='flex flex-col items-center'>
+            <h1 className='text-neutral-600 text-4xl font-semibold mb-[1rem]'>Register</h1>
 
-                <h1 className='text-4xl font-semibold mb-[1rem]'>Register</h1>
+            <div>
+                <Form onSubmit={handleSubmit} className='select-none w-[360px] flex flex-col items-center gap-[10px]'>
 
-                <Input id='name-input' type='name' placeholder='House name' className='entrance-input'>House name</Input>
 
-                <Input id='email-input' type='email' placeholder='Email' className='entrance-input'>Email</Input>
+                    <Input id='name-input' type='name' placeholder='House name' className='entrance-input'>House name</Input>
 
-                <Input id='password-input' type='Password' placeholder='Password' className='entrance-input'>Password</Input>
+                    <Input id='email-input' type='email' placeholder='Email' className='entrance-input'>Email</Input>
 
-                <Button type='submit' className='cursor-pointer mt-5 p-[1.8rem] border-none rounded-md text-lg text-white bg-amber-400 w-[216px] h-[30px] flex justify-center items-center'>Register</Button>
-            </Form>
+                    <Input id='password-input' type='Password' placeholder='Password' className='entrance-input'>Password</Input>
 
-            <nav className='text-sm flex justify-center gap-4 w-[100%] pt-5'>
-                <p>Already have an account?</p>
-                <Link className='underline underline-offset-2' onClick={handleLoginClick}>Log in</Link>
+                    <Button type='submit' className='flex items-center justify-center w-full py-3 text-lg text-white border-none rounded-md cursor-pointer px-28 bg-amber-400'>Register</Button>
+                </Form>
+            </div>
+
+            <nav className='flex flex-col items-center gap-10 mt-20 text-sm text-center lg:mt-12'>
+                <div className='flex gap-4'>
+                    <p>Already have an account?</p>
+                    <Link className='underline underline-offset-2' onClick={handleLoginClick}>Log in</Link>
+                </div>
             </nav>
-        </article>
+        </div>
+
+        {loading
+            ? <div className='absolute p-5 rounded-md w-72 bg-amber-400 top-70'>
+                <p className='pb-4 font-bold text-center text-white'>Login may take up to 3 minutes, because the server has to initialize. After that it'll be faster.</p>
+
+                <div className='flex justify-center'>
+                    <ClipLoader
+                        color={color}
+                        loading={loading}
+                        size={100}
+                        aria-label="Loading Spinner"
+                        data-testid="loader"
+                    />
+                </div>
+            </div>
+            : ''}
     </Container>
 }
 
